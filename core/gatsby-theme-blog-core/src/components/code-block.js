f3724590eded596fd42c0e2f9ef3785140a97d92
arshad <arshad@chummun.com>
2020-05-05 15:06:36 +0400

    fix: format code

--- core/gatsby-theme-blog-core/src/components/code-block.js
+++ core/gatsby-theme-blog-core/src/components/code-block.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import Highlight, {defaultProps} from 'prism-react-renderer'
+import React from "react"
+import Highlight, { defaultProps } from "prism-react-renderer"
 
-export default ({theme, children, className}) => {
-  const language = className.replace(/language-/, '')
+export default ({ theme, children, className }) => {
+  const language = className.replace(/language-/, "")
   return (
-    <Highlight {...defaultProps} theme={theme} code={children.trim()} language={language}>
-      {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <pre className={className} style={{...style, padding: '20px'}}>
+    <Highlight
+      {...defaultProps}
+      theme={theme}
+      code={children.trim()}
+      language={language}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={{ ...style, padding: "20px" }}>
           {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({line, key: i})}>
+            <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
-                <span key={key} {...getTokenProps({token, key})} />
+                <span key={key} {...getTokenProps({ token, key })} />
               ))}
             </div>
           ))}
